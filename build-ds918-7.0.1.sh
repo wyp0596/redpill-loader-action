fb758b0c63f16866c8b2a049f5a4560ef2988694
sakurawill <sakurawill@foxmail.com>
2021-09-18 16:19:55 +0800

    Update build-ds918-7.0.1.sh

--- build-ds918-7.0.1.sh
+++ build-ds918-7.0.1.sh
@@ -10,17 +10,17 @@ cd ds918-7.0.1
 
 # download redpill
 git clone --depth=1 https://github.com/RedPill-TTG/redpill-lkm.git
-git clone --depth=1 https://github.com/chchia/redpill-load.git
+git clone --depth=1 https://github.com/sakurawill/redpill-load.git
 
 # download syno toolkit
-curl --location "https://sourceforge.net/projects/dsgpl/files/toolkit/DSM7.0/ds.apollolake-7.0.dev.txz/download" --output ds.apollolake-7.0.dev.txz
+curl --location "https://sourceforge.net/projects/dsgpl/files/toolkit/DSM7.0/ds.geminilake-7.0.dev.txz/download" --output ds.geminilake-7.0.dev.txz
 
-mkdir apollolake
-tar -C./apollolake/ -xf ds.apollolake-7.0.dev.txz usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build
+mkdir geminilake
+tar -C./geminilake/ -xf ds.geminilake-7.0.dev.txz usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build
 
 # build redpill-lkm
 cd redpill-lkm
-make LINUX_SRC=../apollolake/usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build
+make LINUX_SRC=../geminilake/usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build
 read -a KVERS <<< "$(sudo modinfo --field=vermagic redpill.ko)" && cp -fv redpill.ko ../redpill-load/ext/rp-lkm/redpill-linux-v${KVERS[0]}.ko || exit 1
 cd ..
 
